2cc1ea1df93ce638b269d7414ebb3ac218dacf16
fj168104 <fengj@jxltech.com>
2018-03-03 22:15:55 +0800

    删除旧doc

--- mr-frontier-admin/src/main/java/com/jk/modules/sys/controller/IndexController.java
+++ mr-frontier-admin/src/main/java/com/jk/modules/sys/controller/IndexController.java
@@ -1,15 +1,21 @@
 package com.jk.modules.sys.controller;
 
+import com.feilong.core.DatePattern;
 import com.jk.common.base.controller.BaseController;
 import com.jk.common.util.ShiroUtils;
 import com.jk.modules.sys.model.Permission;
 import com.jk.modules.sys.model.User;
 import com.jk.modules.sys.service.PermissionService;
+import com.xiaoleilu.hutool.date.DateUtil;
+import com.xiaoleilu.hutool.util.RandomUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.annotation.Resource;
+import java.text.DateFormat;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -59,8 +65,42 @@ public class IndexController extends BaseController{
      * @return
      */
     @GetMapping(value = "/admin/welcome")
-    public String welcome(){
+    public String welcome(ModelMap modelMap){
         log.info("------进入欢迎页-------");
+        int len = 20;
+        String[] dsArray = new String[len];
+        Integer[] num1 = new Integer[len];
+        Integer[] num2 = new Integer[len];
+        Integer[] num3 = new Integer[len];
+        Integer[] num4 = new Integer[len];
+        Integer[] num5 = new Integer[len];
+
+        for(int i = 0; i < len; i++){
+            dsArray[i] = "'" + DateUtil.format(DateUtil.offsiteSecond(DateUtil.date(), i), DatePattern.COMMON_TIME) + "'";
+            num1[i] = RandomUtil.randomInt(30) + 50 + 5;
+            num2[i] = RandomUtil.randomInt(30) + 50 + 10;
+            num3[i] = RandomUtil.randomInt(30) + 50 + 15;
+            num4[i] = RandomUtil.randomInt(30) + 50 + 20;
+            num5[i] = RandomUtil.randomInt(30) + 50 + 25;
+        }
+        String arrX = Arrays.toString(dsArray);
+
+        String nArr1 = Arrays.toString(num1);
+        String nArr2 = Arrays.toString(num2);
+        String nArr3 = Arrays.toString(num3);
+        String nArr4 = Arrays.toString(num4);
+        String nArr5 = Arrays.toString(num5);
+
+        modelMap.put("nameArr", "['邮件营销', '百度信用', '实时计算', '直接访问', '搜索引擎']");
+
+        modelMap.put("arrX", arrX);
+
+        modelMap.put("nArr1", nArr1);
+        modelMap.put("nArr2", nArr2);
+        modelMap.put("nArr3", nArr3);
+        modelMap.put("nArr4", nArr4);
+        modelMap.put("nArr5", nArr5);
+
         return BASE_PATH+"welcome";
     }
 
