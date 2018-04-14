984b7bf0e74148aa2d3a008fbd32eada959ec991
fj168104 <fengj@jxltech.com>
2018-04-14 22:22:32 +0800

    解析工厂 版本管理管理界面

---
+++ mr-frontier-admin/src/main/java/com/jk/modules/oti/mapper/OtiFieldLibraryMapper.java
@@ -0,0 +1,7 @@
+package com.jk.modules.oti.mapper;
+
+import com.jk.common.base.mapper.BaseMapper;
+import com.jk.modules.oti.model.OtiFieldLibrary;
+
+public interface OtiFieldLibraryMapper extends BaseMapper<OtiFieldLibrary> {
+}
